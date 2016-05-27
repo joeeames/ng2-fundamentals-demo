02e25ed676db741e9b4642bcb9bf44b7f7625c8a
Joe Eames <joeeames@gmail.com>
2016-05-27 00:37:53 -0600

    added filtering

--- app/events/event-details.component.ts
+++ app/events/event-details.component.ts
@@ -12,6 +12,10 @@ import { UpvoteComponent } from './upvote.component';
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   voted: boolean;
+  sortedByName: boolean;
+  sortedByVote: boolean;
+  visibleSessions: Session[] = [];
+  activeFilter: string;
   
   constructor(private eventService: EventService,
     private routeParams: RouteParams) {}
@@ -20,8 +24,38 @@ export class EventDetailsComponent implements OnInit {
     this.eventService.getEvent(+this.routeParams.get('id'))
       .subscribe(event => {
         this.event = event;
+        this.filterSessions('all');
+        this.sortByName();
+        
       })
     this.voted = false;
+  }
+  
+  sortByName() {
+    this.visibleSessions.sort(sortByNameAsc);
+    this.sortedByVote = false;
+    this.sortedByName = true;
+  }
+  
+  sortByVoteCount() {
+    this.visibleSessions.sort(sortByVotesDesc);
+    this.sortedByVote = true;
+    this.sortedByName = false;
+  }
+  
+  filterSessions(filter) {
+    this.activeFilter = filter;
+    if(filter === 'all') {
+      this.visibleSessions = this.event.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.event.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
+    }
+  }
+  
+  isActiveFilter(filter) {
+    return filter === this.activeFilter;
   }
   
   upVoteSession(session: Session) {
@@ -37,4 +71,14 @@ export class EventDetailsComponent implements OnInit {
       this.voted = true;
     }
   }
+}
+
+function sortByNameAsc(s1: Session, s2: Session) {
+  if(s1.name > s2.name) return 1
+  else if(s1.name === s2.name) return 0
+  else return -1
+}
+
+function sortByVotesDesc(s1: Session, s2: Session) {
+  return s2.voteCount - s1.voteCount
 }
--- server/database/events.js
+++ server/database/events.js
@@ -23,12 +23,30 @@ var events = [
       },
       {
         id: 2,
+        name: "Getting the most out of your dev team",
+        presenter: "Jeff Cross",
+        duration: 4,
+        level: "Intermediate",
+        abstract: "We all know that our dev teams work hard, but with the right management they can be even more productive, without overworking them. In this session I'll show you how to get the best results from the talent you already have on staff.",
+        voteCount: 7
+      },
+      {
+        id: 3,
         name: "Angular Performance Metrics",
         presenter: "Rob Wormald",
         duration: 4,
         level: "Advanced",
         abstract: "Angular 2 Performance is hot. In this session, we'll see all about it",
-        voteCount: 7
+        voteCount: 2
+      },
+      {
+        id: 4,
+        name: "Basics of Angular 2",
+        presenter: "John Papa",
+        duration: 4,
+        level: "Beginner",
+        abstract: "It's time to learn the basics of Angular 2. This talk will give you everything you need to know about Angular 2 to get started with it today.",
+        voteCount: 2
       }
     ]
   },
