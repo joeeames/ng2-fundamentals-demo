1dbafcc610b2bf08288d344489ddd2a8bddc3aed
Jim Cooper <jim@cooperplanet.com>
2016-06-02 23:23:44 -0600

    Toggle add session mode

---
+++ app/events/event-details/create-session.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Session } from '../shared/index';
+
+@Component({
+  moduleId: module.id,
+  selector: 'create-session',
+  templateUrl: './create-session.component.html'
+})
+export class CreateSessionComponent  {
+  constructor() {
+    
+  }
+}
--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -2,17 +2,20 @@ import { Component, Input, OnInit } from '@angular/core';
 import { EventService, Event, Session } from '../shared/index';
 import { RouteParams } from '@angular/router-deprecated';
 import { SessionListComponent } from './session-list.component'
+import { CreateSessionComponent } from './create-session.component'
 
 @Component({
   moduleId: module.id,
   selector: 'event-details',
   templateUrl: 'event-details.component.html',
-  directives: [SessionListComponent]
+  styles: ['a {cursor:pointer}'],
+  directives: [SessionListComponent, CreateSessionComponent]
 })
 export class EventDetailsComponent implements OnInit {
   event: Event
   sortBy: string = 'votes'
   filterBy: string = 'all'
+  addMode: boolean = false
     
   constructor(private eventService: EventService,
     private routeParams: RouteParams) {}
@@ -20,5 +23,9 @@ export class EventDetailsComponent implements OnInit {
   ngOnInit() {
     this.eventService.getEvent(+this.routeParams.get('id'))
       .subscribe(event => this.event = event)
+   }
+   
+   addSession() {
+     this.addMode = true
    }
 }
