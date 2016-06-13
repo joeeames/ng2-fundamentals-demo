f942bb90ce4e5f49b409b24374e24f559069024c
Joe Eames <joeeames@gmail.com>
2016-06-13 14:36:30 -0600

    added multi slot transclusion demo

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -2,31 +2,22 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'collapsible-well',
-    template:`
-<div (click)="toggleContent()" class="well pointable">
-  <h4 class="well-title">{{title}}</h4>
-  <ng-content *ngIf="visible"></ng-content>
-</div>
+  template:`
+    <div (click)="toggleContent()" class="well pointable">
+      <h4>
+        <ng-content select="well-title"></ng-content>
+      </h4>
+      <ng-content select="well-body" *ngIf="visible"></ng-content>
+    </div>
   `,
-//   template:`
-// <div (click)="toggleContent()" class="well pointable">
-//   <h4 class="well-title">{{title}}
-//     <ng-content select="title"></ng-content>
-//   </h4>
-//   <ng-content select="body" *ngIf="visible"></ng-content>
-// </div>
-//   `,
 })
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
   
   toggleContent() {
-    console.log('toggling', this.visible);
     this.visible = !this.visible;
   }
 }
-
 
 
 // SINGLE CONTENT PROJECTION SLOT
