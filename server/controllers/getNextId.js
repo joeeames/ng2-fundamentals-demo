5a1130d40000e7175041f30098ed32389918ed89
Jim Cooper <jim@cooperplanet.com>
2016-06-17 21:50:24 -0600

    Move dependencies to npm packages

--- server/controllers/getNextId.js
+++
@@ -1,7 +0,0 @@
-module.exports = function getNextId(collection) {
-  var nextId = 1;
-  collection.forEach(function(item) {
-    nextId = item.id >= nextId ? item.id + 1 : nextId;
-  });
-  return nextId;
-}
